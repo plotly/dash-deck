1a9b4ba93cf4c4f74cc345c6e2e20933116ca8df
xhlulu <xinghan@plot.ly>
2020-08-18 21:48:54 -0400

    Initial version of the component

--- src/demo/App.js
+++ src/demo/App.js
@@ -1,30 +1,38 @@
 /* eslint no-magic-numbers: 0 */
 import React, {Component} from 'react';
-
 import { DeckGL } from '../lib';
 
+const json = { 
+    "description": "A minimal deck.gl example rendering a circle with text", 
+    "initialViewState": { "longitude": -122.45, "latitude": 37.8, "zoom": 12 }, 
+    "layers": [ 
+        { 
+            "@@type": "ScatterplotLayer", 
+            "data": [ { "position": [ -122.45, 37.8 ] } ], 
+            "getFillColor": [ 255, 0, 0, 255 ], 
+            "getRadius": 1000 
+        },
+        { 
+            "@@type": "TextLayer", 
+            "data": [ { "position": [ -122.45, 37.8 ], 
+            "text": "Hello World" } ] 
+        } 
+    ] 
+};
+
+
+// Copy paste your token here:
+const MAPBOX_ACCESS_TOKEN = "pk.eyJ1IjoieGhsdWx1IiwiYSI6ImNrYW14cWVyMTFmNGIyeHBlbTZpb2toc28ifQ.JcTUr2qlBBEgEYQMtF_kXA";
+
+
 class App extends Component {
-
-    constructor() {
-        super();
-        this.state = {
-            value: ''
-        };
-        this.setProps = this.setProps.bind(this);
-    }
-
-    setProps(newProps) {
-        this.setState(newProps);
-    }
-
     render() {
         return (
-            <div>
-                <DeckGL
-                    setProps={this.setProps}
-                    {...this.state}
-                />
-            </div>
+            <DeckGL 
+                json={json} 
+                id={"deck-gl"} 
+                mapboxApiAccessToken={MAPBOX_ACCESS_TOKEN}
+            />
         )
     }
 }
