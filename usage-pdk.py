ad4b7602043edc8bef6ae0d6983006f09889b36a
xhlulu <xinghan@plot.ly>
2020-08-18 21:49:13 -0400

    Created simple and pdk examples

---
+++ usage-pdk.py
@@ -0,0 +1,57 @@
+import os
+import json
+
+import dash
+import dash_deck
+import dash_html_components as html
+import pydeck as pdk
+
+mapbox_api_token = os.getenv("MAPBOX_API_KEY")
+
+# 2014 locations of car accidents in the UK
+UK_ACCIDENTS_DATA = ('https://raw.githubusercontent.com/uber-common/'
+                     'deck.gl-data/master/examples/3d-heatmap/heatmap-data.csv')
+
+# Define a layer to display on a map
+layer = pdk.Layer(
+    'HexagonLayer',
+    UK_ACCIDENTS_DATA,
+    get_position=['lng', 'lat'],
+    auto_highlight=True,
+    elevation_scale=50,
+    pickable=True,
+    elevation_range=[0, 3000],
+    extruded=True,
+    coverage=1
+)
+
+# Set the viewport location
+view_state = pdk.ViewState(
+    longitude=-1.415,
+    latitude=52.2323,
+    zoom=6,
+    min_zoom=5,
+    max_zoom=15,
+    pitch=40.5,
+    bearing=-27.36
+)
+
+# Render
+r = pdk.Deck(layers=[layer], initial_view_state=view_state)
+
+
+app = dash.Dash(__name__)
+
+app.layout = html.Div(
+    [
+        dash_deck.DeckGL(
+            json.loads(r.to_json()),
+            id="deck-gl",
+            mapboxApiAccessToken=mapbox_api_token
+        )
+    ]
+)
+
+
+if __name__ == "__main__":
+    app.run_server(debug=True)
--- usage.py
+++ usage.py
@@ -1,24 +1,41 @@
-import dash_deck
+import os
+
 import dash
-from dash.dependencies import Input, Output
+import dash_deck
 import dash_html_components as html
+
+
+data = {
+    "description": "A minimal deck.gl example rendering a circle with text",
+    "initialViewState": {"longitude": -122.45, "latitude": 37.8, "zoom": 12},
+    "layers": [
+        {
+            "@@type": "ScatterplotLayer",
+            "data": [{"position": [-122.45, 37.8]}],
+            "getFillColor": [255, 0, 0, 255],
+            "getRadius": 1000,
+        },
+        {
+            "@@type": "TextLayer",
+            "data": [{"position": [-122.45, 37.8], "text": "Hello World"}],
+        },
+    ],
+}
+
+mapbox_api_token = os.getenv("REACT_APP_MAPBOX_API_KEY")
 
 app = dash.Dash(__name__)
 
-app.layout = html.Div([
-    dash_deck.DeckGL(
-        id='input',
-        value='my-value',
-        label='my-label'
-    ),
-    html.Div(id='output')
-])
+app.layout = html.Div(
+    [
+        dash_deck.DeckGL(
+            data,
+            id="deck-gl",
+            mapboxApiAccessToken=mapbox_api_token
+        )
+    ]
+)
 
 
-@app.callback(Output('output', 'children'), [Input('input', 'value')])
-def display_output(value):
-    return 'You have entered {}'.format(value)
-
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run_server(debug=True)
