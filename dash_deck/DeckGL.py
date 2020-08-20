fd73c59fddfee4b8f766394fb9d5e7b7e4f970d8
xhlulu <xinghan@plot.ly>
2020-08-20 19:32:52 -0400

    npm run build

--- dash_deck/DeckGL.py
+++ dash_deck/DeckGL.py
@@ -15,30 +15,30 @@ pdk.Deck(...).to_json(). Both a Python dictionary and a JSON-string your map is 
 - id (string; optional): The ID used to identify this component in Dash callbacks.
 - mapboxKey (string; optional): `mapboxKey` (text): You will need a mapbox token to use deck.gl. Please create a mapbox
 and follow the instructions here: https://docs.mapbox.com/help/how-mapbox-works/access-tokens/
-- clickEvent (dict; optional): This prop is updated when an element in the map is clicked. This contains
+- clickEvent (dict; optional): Read-only prop. This prop is updated when an element in the map is clicked. This contains
 the original gesture event (in JSON).
-- clickInfo (dict; optional): This prop is updated when an element in the map is clicked. This contains
+- clickInfo (dict; optional): Read-only prop. This prop is updated when an element in the map is clicked. This contains
 the picking info describing the object being clicked.
 
 Complete description here:
 https://deck.gl/docs/developer-guide/interactivity#the-picking-info-object
-- hoverEvent (dict; optional): This prop is updated when an element in the map is hovered. This contains
+- hoverEvent (dict; optional): Read-only prop. This prop is updated when an element in the map is hovered. This contains
 the original gesture event (in JSON).
-- hoverInfo (dict; optional): This prop is updated when an element in the map is hovered. This contains
+- hoverInfo (dict; optional): Read-only prop. This prop is updated when an element in the map is hovered. This contains
 the picking info describing the object being hovered.
 
 Complete description here:
 https://deck.gl/docs/developer-guide/interactivity#the-picking-info-object
-- dragStartEvent (dict; optional): This prop is updated when the user starts dragging on the canvas. This contains
+- dragStartEvent (dict; optional): Read-only prop. This prop is updated when the user starts dragging on the canvas. This contains
 the original gesture event (in JSON).
-- dragStartInfo (dict; optional): This prop is updated when the user starts dragging on the canvas. This contains
+- dragStartInfo (dict; optional): Read-only prop. This prop is updated when the user starts dragging on the canvas. This contains
 the picking info describing the object being dragged.
 
 Complete description here:
 https://deck.gl/docs/developer-guide/interactivity#the-picking-info-object
-- dragEndEvent (dict; optional): This prop is updated when the user releases from dragging the canvas. This contains
+- dragEndEvent (dict; optional): Read-only prop. This prop is updated when the user releases from dragging the canvas. This contains
 the original gesture event (in JSON).
-- dragEndInfo (dict; optional): This prop is updated when the user releases from dragging the canvas. This contains
+- dragEndInfo (dict; optional): Read-only prop. This prop is updated when the user releases from dragging the canvas. This contains
 the picking info describing the object being dragged.
 
 Complete description here:
